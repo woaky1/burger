5db914a8dffb1a7516ff24fe363d2ceccb9f0819
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-15 20:01:52 -0500

    added some initial files and file structures.

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+var PORT = process.env.PORT || 3001
+
+// These lines allow to Express to parse info placed in a url as well as JSON data.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});

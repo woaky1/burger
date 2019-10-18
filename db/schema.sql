450c3c4f27ead720196be77e57d0df880e6aea64
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-18 17:22:58 -0500

    Added default value for devoured.

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burgers_db;
 CREATE TABLE burgers(
   id INT AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(300) NOT NULL,
-  devoured BOOLEAN,
+  devoured BOOLEAN DEFAULT false,
   createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY(id)
 );
